cd0e247f217f65a39f39951f183dac2f9ea513b1
Jyotimoykathar <jyotimoykatharon@gmail.com>
2025-12-01 15:03:30 +0530

    adding button at thankyou page

--- src/pages/ThankYou.jsx
+++ src/pages/ThankYou.jsx
@@ -1,10 +1,17 @@
+import { Link } from "react-router-dom";
 export default function ThankYou() {
   return (
     <div className="max-w-md mx-auto card tex-center">
       <h1 className="text-3xl font-bold mb-3">Thank You</h1>
-      <p>
+      <p className="mb-4">
         Your message has been sent successfully. We will get back to you soon
       </p>
+      <Link to="/" className="btn-primary inline-flex items-center gap-2">
+        <span className="text-xl font-bold leading-none relative -top-[2px]">
+          &larr;
+        </span>
+        <span>Homepage</span>
+      </Link>
     </div>
   );
 }
